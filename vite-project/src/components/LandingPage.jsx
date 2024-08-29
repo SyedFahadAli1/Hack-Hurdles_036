9501f0d6b34adbed38ab3990e4bed5307d3ff76f
Rutvik020302 <rutvikpethe22@gmail.com>
2024-08-30 00:05:23 +0530

    signup or login code

---
+++ vite-project/src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="wrapper">
+      <h1>Welcome to the Landing Page!</h1>
+      <p>You are successfully logged in.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
